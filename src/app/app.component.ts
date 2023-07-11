de999dc98cba2854bfa1d08f01862fe10f876d76
Joshua Daniel <joshua_dup@hotmail.com>
2023-07-11 17:53:15 -0500

    app-empleados permite el registroo mediante un formulario y con sus directivas podemos manejar y reflejar esta innfoormacion en el template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Empleado } from './models/empleado.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app-empleados';
+  title = 'Listado de Empleados';
+
+  empleados: Empleado[] = [
+    new Empleado('Luis', 'Díaz', 'Diseñador', 900),
+    new Empleado('Daniel', 'Jimenez', 'Desarrollador Backend', 1200),
+    new Empleado('Andres', 'Vargas', 'Diseñador Fullstack', 2000),
+    new Empleado('Jesus', 'Potter', 'Scrum Master', 1500),
+  ];
+
+  cuadroNombre: string = '';
+  cuadroApellido: string = '';
+  cuadroCargo: string = '';
+  cuadroSalario: number = 0;
+
+  agregarEmpleado() {
+    let miEmpleado = new Empleado(
+      this.cuadroNombre,
+      this.cuadroApellido,
+      this.cuadroCargo,
+      this.cuadroSalario
+    );
+    this.empleados.push(miEmpleado);
+  }
 }
